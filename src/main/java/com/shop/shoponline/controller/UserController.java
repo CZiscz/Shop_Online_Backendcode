a8c5db2c479077150806c1b1dc7f13e8744a3ea3
astronaut <2426825595@qq.com>
2023-11-11 10:13:48 +0800

    用户登录

--- src/main/java/com/shop/shoponline/controller/UserController.java
+++ src/main/java/com/shop/shoponline/controller/UserController.java
@@ -1,5 +1,15 @@
 package com.shop.shoponline.controller;
 
+import com.shop.shoponline.common.result.Result;
+import com.shop.shoponline.query.UserLoginQuery;
+import com.shop.shoponline.service.UserService;
+import com.shop.shoponline.vo.LoginResultVO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +22,16 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2023-11-08
  */
 @RestController
-@RequestMapping("/shoponline/user")
+@Tag(name="用户模块")
+@RequestMapping("user")
+@AllArgsConstructor
 public class UserController {
+    private final UserService userService;
 
+    @Operation(summary = "微信登录")
+    @PostMapping("/login/wxMin")
+    public Result<LoginResultVO> wxLogin(@RequestBody @Validated UserLoginQuery query) {
+        LoginResultVO userVO = userService.login(query);
+        return Result.ok(userVO);
+    }
 }
